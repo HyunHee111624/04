9c4da5077dde059de8345856757b828552272772
HyunHee111624 <144989249+HyunHee111624@users.noreply.github.com>
2023-10-02 22:16:35 +0900

    1

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int x, y, z, m;
+  int a, b, c;
+  
+  x = 2;
+  z = 1;
+  a = 3;
+  b = 4;
+  c = 5;
+  
+  //y = ax^2 + bx + c
+  //m = (x+y+z) / 3
+  
+  y = (a*x*x) + (b*x) + c;
+  m = (x+y+z)/3;
+  
+  printf("y=%d, m=%d", y, m);
+  getchar();
+  
+	
+  return 0;
+}
